2b5d1b5756d837015d5fb58734718692f447edc3
Jorge Dazzi <jorge.dazzi@gmail.com>
2020-05-16 17:49:05 -0300

    DefaultExceptionHandler

---
+++ src/main/java/br/dazzi/AM53/controller/error/ErrorResponse.java
@@ -0,0 +1,29 @@
+package br.dazzi.AM53.controller.error;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@JsonRootName("error")
+@Getter
+@Setter
+public class ErrorResponse {
+
+    private List<ErrorMessage> error_list;
+
+    @JsonCreator
+    public ErrorResponse() {
+        error_list = new ArrayList<>();
+    }
+    public ErrorResponse(List<ErrorMessage> error_list) {
+        this.error_list = List.copyOf(error_list);
+    }
+    public ErrorResponse(Set<ErrorMessage> error_list) {
+        this.error_list = List.copyOf(error_list);
+    }
+}
---
+++ src/main/java/br/dazzi/AM53/controller/exceptionhandler/DefaultExceptionHandler.java
@@ -0,0 +1,89 @@
+package br.dazzi.AM53.controller.exceptionhandler;
+
+
+import br.dazzi.AM53.controller.error.ErrorMessage;
+import br.dazzi.AM53.controller.error.ErrorResponse;
+import br.dazzi.AM53.domain.exception.APIException;
+import br.dazzi.AM53.domain.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class DefaultExceptionHandler {
+
+    Logger logger = LoggerFactory.getLogger(DefaultExceptionHandler.class);
+
+    //This going to handle any Exception which was not predicted
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleError(Exception exception, HttpServletRequest request) {
+        logger.error("Unknown error", exception);
+        return getDefaultResponseMessage(request);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(NotFoundException notFoundException, HttpServletRequest request) {
+        return buildErrorResponse(notFoundException, HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleSpringValidation(MethodArgumentNotValidException methodArgumentNotValidException, HttpServletRequest request) {
+
+        Set<ErrorMessage> msgs = methodArgumentNotValidException
+                .getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(
+                        error -> new ErrorMessage(
+                                HttpStatus.BAD_REQUEST.value(),
+                                HttpStatus.BAD_REQUEST.name(),
+                                error.getDefaultMessage()
+                        )
+                ).collect(Collectors.toSet());
+
+        return new ResponseEntity<>(
+                new ErrorResponse(msgs),
+                HttpStatus.UNPROCESSABLE_ENTITY
+        );
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleSpringKeys(DataIntegrityViolationException exception, HttpServletRequest request) {
+
+        String[] field = exception.getMostSpecificCause().getLocalizedMessage().split("\"");
+        List<ErrorMessage> msgs = List.of(new ErrorMessage(
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.name(),
+                field.length > 1 ? field[1] : exception.getLocalizedMessage())
+        );
+
+        return new ResponseEntity<>(
+                new ErrorResponse(msgs),
+                HttpStatus.UNPROCESSABLE_ENTITY
+        );
+    }
+
+    private ResponseEntity<ErrorResponse> buildErrorResponse(APIException exception, HttpStatus httpStatus, HttpServletRequest request) {
+        try {
+            List<ErrorMessage> errorMessages = List.of(new ErrorMessage(exception.getCode(), exception.getDescription(), exception.getMessage()));
+            return new ResponseEntity<>(new ErrorResponse(errorMessages), httpStatus);
+        } catch (Exception e) {
+            return getDefaultResponseMessage(request);
+        }
+    }
+
+    private ResponseEntity<ErrorResponse> getDefaultResponseMessage(HttpServletRequest request) {
+        ErrorMessage errorMessage = new ErrorMessage(500, "internal_error", "Internal Error");
+        return new ResponseEntity<>(new ErrorResponse(List.of(errorMessage)), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
